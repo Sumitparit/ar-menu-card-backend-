14b93369487e96868f43f06f9e7d6fadc4702870
Ashishkuldeep23 <ashishkuldeep6@gmail.com>
2024-05-07 21:51:37 +0530

    cors list updated

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose.connect(dbString)
 // // // CORS code ---->
 app.use(cors({
     credentials: true,
-    origin: [`${process.env.FRONTEND_URL}`, 'https://restaurant-ar-app2-zx4c.vercel.app/']
+    origin: [`${process.env.FRONTEND_URL}`, 'https://restaurant-ar-app2-zx4c.vercel.app/', 'https://ar-menu-third.vercel.app/']
 }))
 
 
